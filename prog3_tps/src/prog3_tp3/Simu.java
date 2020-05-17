448edb9c7e9c0e3018977dfd0771a30345cadd86
joaquin.mancini <mancinijoaquinandres@gmail.com>
2020-05-17 11:55:01 -0300

    Working sol to problem requested in Entregable3

--- prog3_tps/src/prog3_tp3/Simu.java
+++ prog3_tps/src/prog3_tp3/Simu.java
@@ -6,9 +6,7 @@ public class Simu {
 
 	private static HashMap<Integer, Task<Integer>> tasks = new HashMap<Integer, Task<Integer>>();
 	private static LinkedList<Integer> sol = new LinkedList<Integer>();
-	private static int time;
-	private final static Integer NOTVISITED = 0;
-	private final static Integer VISITED = 1;
+	private static int maxTime;
 
 	public static void main(String[] args) {
 		DirectedGraph<Integer> dg1 = new DirectedGraph<Integer>();
@@ -24,7 +22,7 @@ public class Simu {
 		addTask(dg1, 8, 3);
 		addTask(dg1, 9, 2);
 		addTask(dg1, 10, 3);
-		addTask(dg1, 11, 1);
+		addTask(dg1, 11, 25);
 		addTask(dg1, 12, 5);
 		joinTasks(dg1, 0, 1, 0);
 		joinTasks(dg1, 0, 2, 0);
@@ -41,58 +39,45 @@ public class Simu {
 		joinTasks(dg1, 8, 9, 4);
 		joinTasks(dg1, 11, 12, 9);
 		joinTasks(dg1, 9, 10, 1);
-		Iterator<Integer> vIt = dg1.getVerts();
-		while (vIt.hasNext()) {
-			System.out.println(vIt.next());
-		}
-		Iterator<Arc<Integer>> aIt = dg1.getArcs();
-		while (aIt.hasNext()) {
-			Arc<Integer> a = aIt.next();
-			System.out.println(a.getVertOrg() + " -> " + a.getVertDest());
-		}
-
 		DFS(dg1);
+		System.out.println(sol);
+		System.out.println(tasks);
 
 	}
 
 	// metodo de entrada a DFS, verifica vertices no fueron visitados
 	public static void DFS(DirectedGraph<Integer> dg) {
-		// Setea todos a no visitados
-		setNotVisited(dg);
-		time = 0;
-		Iterator<Integer> vIt = dg.getVerts();
-		// recorre cada vertice
-		while (vIt.hasNext()) {
-			int v = vIt.next();
-			// si no a sido visitado entra a DFS_visit
-			if (tasks.get(v).getState() == NOTVISITED) {
-				DFS_visit(dg, v);
-			}
-		}
+		LinkedList<Integer> aux = new LinkedList<Integer>();
+		Integer first = 0;
+		Integer cS = 0;
+		sol.clear();
+		maxTime = 0;
+		DFS_visit(dg, aux, first, cS);
+		sol.push(first);
 	}
 
 	// DFS recursivo, analiza todos los vertices adyacentes a cierto vertice
-	public static void DFS_visit(DirectedGraph<Integer> dg, Integer v) {
-		// Setea vertice a visitado
-		tasks.get(v).setState(VISITED);
-		time++;
-		// Setea tiempo de entrada a vertice
-		tasks.get(v).setD(1);
+	public static void DFS_visit(DirectedGraph<Integer> dg, LinkedList<Integer> p, Integer v, Integer s) {
 		Iterator<Arc<Integer>> ayIt = dg.getArcs(v);
-		// recorre los adyacentes a ese vertice
-		while (ayIt.hasNext()) {
-			Arc<Integer> a = ayIt.next();
-			// si no a sido visitado entra a DFS_visit
-			if (tasks.get(a.getVertDest()).getState() == NOTVISITED) {
-				DFS_visit(dg, a.getVertDest());
+		if (!ayIt.hasNext()) {
+			if (s > maxTime) {
+				maxTime = s;
+				sol.clear();
+				sol.addAll(p);
 			}
 		}
-		time++;
-		// Setea tiempo de salida de vertice
-		tasks.get(v).setF(time);
+		while (ayIt.hasNext()) {
+			Arc<Integer> a = ayIt.next();
+			p.add(a.getVertDest());
+			s += a.getTag() + tasks.get(v).getDuration();
+			DFS_visit(dg, p, a.getVertDest(), s);
+			p.removeLast();
+			s -= a.getTag() + tasks.get(v).getDuration();
+		}
 	}
 
-	// Añade vertices a grafo y tareas a hashmap, para separar la logica
+	// Añade vertices a grafo y tareas a hashmap, para separar el problema de la
+	// implementacion de grafo
 	public static void addTask(DirectedGraph<Integer> dg, Integer id, Integer task) {
 		dg.addVert(id);
 		tasks.put(id, new Task<Integer>(task));
@@ -101,19 +86,6 @@ public class Simu {
 	// Une vertices, o "tareas", mediante arcos
 	public static void joinTasks(DirectedGraph<Integer> dg, Integer id1, Integer id2, Integer dur) {
 		dg.addArc(id1, id2, dur);
-	}
-
-	// Setea todas las tareas en BLANCO (vertices no visitados)
-	public static void setNotVisited(DirectedGraph<Integer> dg) {
-		Iterator<Integer> vIt = dg.getVerts();
-		while (vIt.hasNext()) {
-			Integer v = vIt.next();
-			tasks.get(v).setState(NOTVISITED);
-		}
-	}
-
-	public static void setState(Integer t, int s) {
-		tasks.get(t).setState(s);
 	}
 
 }
--- prog3_tps/src/prog3_tp3/Task.java
+++ prog3_tps/src/prog3_tp3/Task.java
@@ -2,14 +2,10 @@ package prog3_tp3;
 
 public class Task<T> {
 	private Integer state;
-	private Integer d;
-	private Integer f;
 	private Integer duration;
 
 	public Task(Integer duration) {
 		this.state = 0;
-		this.d = 0;
-		this.f = 0;
 		this.duration = duration;
 	}
 
@@ -19,22 +15,6 @@ public class Task<T> {
 
 	public void setState(Integer state) {
 		this.state = state;
-	}
-
-	public Integer getD() {
-		return d;
-	}
-
-	public void setD(Integer d) {
-		this.d = d;
-	}
-
-	public Integer getF() {
-		return f;
-	}
-
-	public void setF(Integer f) {
-		this.f = f;
 	}
 
 	public Integer getDuration() {
